a56a951c5eb35359d51f37402462395485e0159c
lathiyaharshit <lathiyaharshit19@gmail.com>
2025-04-19 17:40:49 +0530

    ex: add toast and change icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,14 +6,15 @@ import { Link } from "react-router-dom";
 
 const SidebarContext = createContext();
 
-export default function Sidebar({children }) {
+export default function Sidebar({ children }) {
     const [expanded, setExpanded] = useState(true)
     return (
         <>
             <aside className="h-screen">
                 <nav className="h-full flex flex-col bg-white border-r shadow-sm">
                     <div className="p-4 pb-2 flex justify-between items-center">
-                        <img src={logo} className={`overflow-hidden transition-all ${expanded ? "w-32" : "w-0"}`} />
+                        <img src='https://cdn-icons-png.flaticon.com/512/8910/8910710.png' className={`overflow-hidden transition-all ${expanded ? "w-16" : "w-0"}`} />
+                        <p className={`${expanded?'text-base font-extrabold text-amber-900 ' :'hidden'}`}>Expense Manager</p>
                         <button onClick={() => setExpanded((curr) => !curr)} className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
                             {expanded ? <ChevronFirst /> : <ChevronLast />}
                         </button>
@@ -41,27 +42,27 @@ export default function Sidebar({children }) {
     )
 }
 
-export function SidebarItem({ icon, text,to, active, alert }) {    
+export function SidebarItem({ icon, text, to, active, alert }) {
     const { expanded } = useContext(SidebarContext)
     return (
         <Link to={to}>
-      <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group
+            <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group
         ${active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800" : "hover:bg-indigo-50 text-gray-600"}`}>
 
-        {icon}
-        <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>{text}</span>
+                {icon}
+                <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>{text}</span>
 
-        {alert && (
-          <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"}`}></div>
-        )}
+                {alert && (
+                    <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"}`}></div>
+                )}
 
-        {!expanded && (
-          <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all
+                {!expanded && (
+                    <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all
             group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
-            {text}
-          </div>
-        )}
-      </li>
-    </Link>
+                        {text}
+                    </div>
+                )}
+            </li>
+        </Link>
     )
 }
--- src/pages/Expanse.jsx
+++ src/pages/Expanse.jsx
@@ -1,16 +1,9 @@
 import { useEffect, useState } from 'react';
 import { db } from '../firebase';
-import {
-    collection,
-    addDoc,
-    getDocs,
-    deleteDoc,
-    doc,
-    updateDoc,
-    getDoc,
-} from 'firebase/firestore';
+import { collection, addDoc, getDocs, deleteDoc, doc, updateDoc, getDoc } from 'firebase/firestore';
 import { Dialog } from 'primereact/dialog';
-import { PlusIcon } from 'lucide-react';
+import { DeleteIcon, PlusIcon, Trash2Icon } from 'lucide-react';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Expense = () => {
     const [budgets, setBudgets] = useState([]);
@@ -18,13 +11,15 @@ const Expense = () => {
     const [activeBudgetDialog, setActiveBudgetDialog] = useState(false);
 
     const [category, setCategory] = useState('');
-    const [icon, setIcon] = useState('📝');
-    const [total, setTotal] = useState(100);
+    const [icon, setIcon] = useState('');
+    const [total, setTotal] = useState('');
     const [color, setColor] = useState('bg-gray-500');
 
     const [expenseName, setExpenseName] = useState('');
     const [expenseAmount, setExpenseAmount] = useState('');
     const [expenses, setExpenses] = useState([]);
+    const [expenseDate, setExpenseDate] = useState("");
+
 
     // Fetch budgets
     useEffect(() => {
@@ -42,7 +37,7 @@ const Expense = () => {
 
     const handleCreateNewBudget = async () => {
         if (!category.trim()) {
-            alert('Please enter a category.');
+            toast.info("Please enter a category.!");
             return;
         }
 
@@ -63,6 +58,7 @@ const Expense = () => {
             setTotal(100);
             setColor('bg-gray-500');
             setActiveBudgetDialog(false);
+            toast.success('Budget Added Successfully')
         } catch (error) {
             console.error('Error adding budget:', error);
         }
@@ -73,8 +69,9 @@ const Expense = () => {
             const budgetRef = doc(db, 'budgets', id);
             await deleteDoc(budgetRef);
             setBudgets((prevBudgets) => prevBudgets.filter((budget) => budget.id !== id));
+            toast.success('Budget Delete Successfully')
         } catch (error) {
-            console.error('Error removing budget:', error);
+            toast.error('Error removing budget:', error)
         }
     };
 
@@ -91,44 +88,88 @@ const Expense = () => {
 
     const handleAddExpense = async () => {
         if (!expenseName.trim() || !expenseAmount) {
-            alert('Please fill in both fields.');
+            toast.info("Please fill in both fields.");
             return;
         }
 
-        const newExpense = {
-            name: expenseName,
-            amount: parseFloat(expenseAmount),
-            date: new Date().toISOString(),
-        };
+        // const newExpense = {
+        //     id:,
+        //     name: expenseName,
+        //     amount: parseFloat(expenseAmount),
+        //     date: expenseDate ? expenseDate : new Date().toISOString(),
+        // };
 
         try {
             const expensesRef = collection(db, 'budgets', activeBudget.id, 'expenses');
-            await addDoc(expensesRef, newExpense);
-
+            const expenseDocRef = await addDoc(expensesRef, {
+                name: expenseName,
+                amount: parseFloat(expenseAmount),
+                date: expenseDate ? expenseDate : new Date().toISOString(),
+            });
+    
+            const newExpense = {
+                id: expenseDocRef.id, // Use the auto-generated document ID
+                name: expenseName,
+                amount: parseFloat(expenseAmount),
+                date: expenseDate ? expenseDate : new Date().toISOString(),
+            };
+    
+            // Update the "spent" field in the corresponding budget
             const updatedSpent = activeBudget.spent + newExpense.amount;
             const budgetRef = doc(db, 'budgets', activeBudget.id);
             await updateDoc(budgetRef, { spent: updatedSpent });
-
             setExpenses(prev => [...prev, newExpense]);
             setActiveBudget(prev => ({ ...prev, spent: updatedSpent }));
-
             setExpenseName('');
             setExpenseAmount('');
+            setExpenseDate('')
             fetchBudgets();
+            toast.success('Expense Added Successfully')
         } catch (error) {
-            console.error('Error adding expense:', error);
+            toast.error('Error adding expense:', error);
         }
     };
 
+    const handleDeleteExpense = async (expenseId) => {        
+        const confirmDelete = window.confirm("Are you sure you want to delete this expense?"); 
+        if(confirmDelete){
+            try {
+                await deleteDoc(doc(db, "budgets", activeBudget.id, "expenses", expenseId));
+                const expensesRef = collection(db, "budgets", activeBudget.id, "expenses");
+                const querySnapshot = await getDocs(expensesRef);
+                const remainingExpenses = querySnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                const updatedSpent = remainingExpenses.reduce((total, expense) => total + expense.amount, 0);
+
+                // Update the budget's "spent" field in Firestore
+                const budgetRef = doc(db, "budgets", activeBudget.id);
+                await updateDoc(budgetRef, { spent: updatedSpent });
+        
+                // Update states to reflect changes
+                setExpenses(remainingExpenses); // Update local expenses state
+                setActiveBudget((prev) => ({ ...prev, spent: updatedSpent })); 
+                setExpenses((prev) => prev.filter((expense) => expense.id !== expenseId));
+                toast.success("Expense deleted successfully!");
+            } catch (error) {
+                console.error("Error deleting expense:", error);
+                toast.error("Failed to delete expense. Please try again.");
+            }
+        } else{
+            return
+        }      
+    };
+    
     return (
         <>
             <div className="min-h-screen bg-white p-6">
+                <ToastContainer />
                 <h1 className="text-2xl font-bold mb-6">My Budgets</h1>
-
                 {!activeBudget ? (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <div className="w-full max-w-sm mx-auto p-6 bg-blue-100 rounded-lg shadow-md flex flex-col items-center justify-center hover:bg-blue-200 transition-all cursor-pointer" onClick={() => setActiveBudgetDialog(true)}>
-                            <PlusIcon className='h-8 w-8 text-gray-700 mb-2'/>
+                            <PlusIcon className='h-8 w-8 text-gray-700 mb-2' />
                             <span className="text-lg font-semibold text-gray-700">Create New Budget</span>
                         </div>
                         {budgets.map((budget) => (
@@ -136,13 +177,9 @@ const Expense = () => {
                                 <div className="flex items-center gap-3 mb-2">
                                     <span className="text-2xl">{budget.icon}</span>
                                     <div className="text-lg font-semibold">{budget.category}</div>
-                                    <button
-                                        onClick={() => handleRemoveBudget(budget.id)}
-                                        className="ml-auto text-red-500 hover:text-red-700"
-                                    >
-                                        Delete
+                                    <button onClick={() => handleRemoveBudget(budget.id)} className="ml-auto text-red-500 hover:text-red-700">
+                                        <Trash2Icon />
                                     </button>
-
                                 </div>
                                 <div className="space-y-3">
                                     <div>
@@ -205,7 +242,7 @@ const Expense = () => {
                             </div>
 
                             {/* Add Expense Box */}
-                            <div className="w-full max-w-md h-64 border border-purple-300 rounded-lg bg-purple-50 shadow-lg p-6">
+                            <div className="w-full max-w-md h-74 border border-purple-300 rounded-lg bg-purple-50 shadow-lg p-6">
                                 <h2 className="font-semibold text-lg mb-4 text-purple-600">Add Expense</h2>
                                 <input
                                     type="text"
@@ -219,6 +256,12 @@ const Expense = () => {
                                     placeholder="Amount"
                                     value={expenseAmount}
                                     onChange={(e) => setExpenseAmount(e.target.value)}
+                                    className="border border-gray-300 rounded w-full mb-4 p-2 focus:outline-none focus:border-purple-600 hover:border-purple-600 transition-all duration-300"
+                                />
+                                <input
+                                    type="date"
+                                    value={expenseDate}
+                                    onChange={(e) => setExpenseDate(e.target.value)}
                                     className="border border-gray-300 rounded w-full mb-4 p-2 focus:outline-none focus:border-purple-600 hover:border-purple-600 transition-all duration-300"
                                 />
                                 <button
@@ -242,14 +285,31 @@ const Expense = () => {
                                             <th className="border border-gray-300 p-2 text-left">Date</th>
                                             <th className="border border-gray-300 p-2 text-left">Name</th>
                                             <th className="border border-gray-300 p-2 text-left">Amount</th>
+                                            <th className="border border-gray-300 p-2 text-left">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         {expenses.map((expense, index) => (
+                                            
                                             <tr key={index} className="hover:bg-gray-100 transition-colors duration-300">
-                                                <td className="border border-gray-300 p-2">{expense.date}</td>
+                                                <td className="border border-gray-300 p-2">
+                                                    {new Intl.DateTimeFormat("en-GB", {
+                                                        day: "2-digit",
+                                                        month: "2-digit",
+                                                        year: "numeric",
+                                                    }).format(new Date(expense.date))}
+                                                </td>
                                                 <td className="border border-gray-300 p-2">{expense.name}</td>
                                                 <td className="border border-gray-300 p-2">₹{expense.amount.toFixed(2)}</td>
+                                                <td className="border border-gray-300 p-2">
+                                                    {/* Delete Icon */}
+                                                    <button
+                                                        onClick={() => handleDeleteExpense(expense.id)}
+                                                        className="text-red-500 hover:text-red-700 transition-colors duration-300"
+                                                    >
+                                                        <Trash2Icon/>
+                                                    </button>
+                                                </td>
                                             </tr>
                                         ))}
                                     </tbody>
@@ -278,7 +338,7 @@ const Expense = () => {
                         onChange={(e) => setIcon(e.target.value)}
                         className="border p-2 rounded w-full"
                     />
-                    <input type="number" placeholder="Total Amount" value={total} onChange={(e) => setTotal(e.target.value)} className="border p-2 rounded w-full"/>
+                    <input type="number" placeholder="Total Amount" value={total} onChange={(e) => setTotal(e.target.value)} className="border p-2 rounded w-full" />
                     <select
                         value={color}
                         onChange={(e) => setColor(e.target.value)}
